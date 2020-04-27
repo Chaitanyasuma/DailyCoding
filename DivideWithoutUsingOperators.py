c31f608a6ed311d19ff01d04ed98661acc2ac6dc
Chaitanyasuma <43496656+Chaitanyasuma@users.noreply.github.com>
2020-04-27 17:49:56 +0530

    Update DivideWithoutUsingOperators.py

--- DivideWithoutUsingOperators.py
+++ DivideWithoutUsingOperators.py
@@ -1,13 +1,26 @@
+'''Implement division of two positive integers without using the division, multiplication, 
+or modulus operators. Return the quotient as an integer, ignoring the remainder.'''
 a = int(input())
 b = int((input()))
+counter = 0
 flag = 0
 if a == b:
 	print("Divisible")
+	flag = 1
 else:
-	while(a > b):
-		flag = flag + b
-		if flag == a:
+	while(a > counter):
+		counter = counter + b
+		if counter == a:
 			print("Divisible")
+			flag = 1
 			break
-
-	
+		#end if
+	#end while
+	if flag == 0:
+		print("Not Divisible")
+#end else
+'''Output:
+1899
+3
+Divisible
+'''
